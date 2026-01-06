36baf947af4dc2a93e865e56a553eead63ec32cd
IMOMDIN <imomdinhakimov1@gmail.com>
2026-01-06 15:12:56 +0500

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,12 +1,18 @@
 import telebot
 import os
+import time
 
 TOKEN = os.getenv("TOKEN")
-
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
-def start(message):
-    bot.send_message(message.chat.id, "Бот работает.")
+def start(msg):
+    bot.send_message(msg.chat.id, "Бот работает стабильно и бесплатно.")
 
-bot.infinity_polling()
+# Работаем ограниченное время, чтобы GitHub Actions не зависал
+start_time = time.time()
+while time.time() - start_time < 55:
+    try:
+        bot.polling(none_stop=True, interval=1, timeout=20)
+    except Exception:
+        time.sleep(5)
